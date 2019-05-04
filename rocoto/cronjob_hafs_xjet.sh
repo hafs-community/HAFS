0e863ea2c95e1df3fbf9a3db103049085784dd06
Bin Liu <Bin.Liu@noaa.gov>
2019-05-04 02:02:23 +0000

    Update cronjob_hafs_xjet.sh.

--- rocoto/cronjob_hafs_xjet.sh
+++ rocoto/cronjob_hafs_xjet.sh
@@ -7,7 +7,7 @@ cd /lfs3/projects/hwrfv3/${USER}/HAFS/rocoto
 dev="-s sites/xjet.ent -f"
 
 #./run_hafs.py ${dev} 2018 06L HISTORY # Florence
- ./run_hafs.py ${dev} 2019042200 00L HISTORY config.EXPT=HAFS config.SUBEXPT=HAFS_jet ../parm/hafs_regional_static.conf
+ ./run_hafs.py ${dev} -t 2019042200 00L HISTORY config.EXPT=HAFS config.SUBEXPT=HAFS_jet ../parm/hafs_regional_static.conf
 
 date
 echo 'cronjob done'
