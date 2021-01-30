845ca6f1b7bedd0eb1a82c1907a9bedd2a18b17f
Bin.Liu <Bin.Liu@noaa.gov>
2021-01-30 12:12:01 -0600

    Update the cronjob driver script cronjob_hafsv0p2a_baseline.sh

--- rocoto/cronjob_hafsv0p2a_baseline.sh
+++ rocoto/cronjob_hafsv0p2a_baseline.sh
@@ -34,10 +34,30 @@ EXPT=$(basename ${HOMEhafs})
 #===============================================================================
 
  # hafsv0p2a baseline
- ${PYTHON3} ./run_hafs.py -t ${dev} 2020082512 00L HISTORY \
-     config.EXPT=${EXPT} config.SUBEXPT=hafsv0p2a_baseline \
+ confopts="config.EXPT=${EXPT} config.SUBEXPT=hafsv0p2a_baseline \
      ../parm/hafsv0p2a_baseline_AL.conf \
-     ../parm/hafs_hycom.conf
+     ../parm/hafs_hycom.conf"
+
+ # Technical test for 2020082506-2020082512 13L2020
+ ${PYTHON3} ./run_hafs.py -t ${dev} 2020082506-2020082512 00L HISTORY ${confopts} \
+     config.run_hrdgraphics=yes config.run_emcgraphics=yes \
+     config.scrub_work=no config.scrub_com=no
+
+## 2020 NATL storm slots
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020060112-2020060812 00L HISTORY ${confopts} # Slot 1.0: 03L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020062112-2020062406 00L HISTORY ${confopts} # Slot 1.1: 04L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020070400-2020070612 00L HISTORY ${confopts} # Slot 1.2: 05L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020070912-2020071100 00L HISTORY ${confopts} # Slot 1.3: 06L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020072112-2020072618 00L HISTORY ${confopts} # Slot 1.4: 07L, 08L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020072806-2020080500 00L HISTORY ${confopts} # Slot 2: 09L, 10L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020081100-2020081612 00L HISTORY ${confopts} # Slot 3: 11L, 12L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020081918-2020082718 00L HISTORY ${confopts} # Slot 4: 13L, 14L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020083112-2020090512 00L HISTORY ${confopts} # Slot 5: 15L, 16L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020090612-2020091118 00L HISTORY ${confopts} # Slot 6: 17Lp1, 18Lp1
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020091200-2020092300 00L HISTORY ${confopts} # Slot 7: 17Lp2, 18Lp2, 19-24L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020100212-2020101018 00L HISTORY ${confopts} # Slot 8: 25L, 26L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020101618-2020102912 00L HISTORY ${confopts} # Slot 9: 27L, 28L
+#${PYTHON3} ./run_hafs.py -t ${dev} 2020101618-2020102912 00L HISTORY ${confopts} # Slot 10: 29-31L # No GFSv16 version input data
 
 #===============================================================================
 
