3aa16f51561ba69094f87605ea225bbcee13db3c
Zhan.Zhang <Zhan.Zhang@noaa.gov>
2021-05-13 14:52:10 -0500

    add command for orion crontab job cronjob_hafs_regional_ensemble.sh

--- rocoto/cronjob_hafs_regional_ensemble.sh
+++ rocoto/cronjob_hafs_regional_ensemble.sh
@@ -13,15 +13,16 @@ date
 #PYTHON3=/opt/intel/intelpython3/bin/python3
 
 # NOAA RDHPCS Jet
-HOMEhafs=/mnt/lfs4/HFIP/hwrf-vd/${USER}/HAFS2021_ensemble
+#HOMEhafs=/mnt/lfs4/HFIP/hwrf-vd/${USER}/HAFS2021_ensemble
 #dev="-s sites/xjet.ent -f"
-dev="-s sites/xjet_ensemble.ent -f"
-PYTHON3=/apps/intel/intelpython3/bin/python3
+#dev="-s sites/xjet_ensemble.ent -f"
+#PYTHON3=/apps/intel/intelpython3/bin/python3
 
 # MSU Orion
-# HOMEhafs=/work/noaa/hwrf/save/${USER}/HAFS
-# dev="-s sites/orion.ent -f"
-# PYTHON3=/apps/intel-2020/intel-2020/intelpython3/bin/python3
+ HOMEhafs=/work/noaa/hwrf/save/${USER}/HAFS
+ HOMEhafs=/work/noaa/hurricane/save/${USER}/H221_hafs_ensemble
+ dev="-s sites/orion_ensemble.ent -f"
+ PYTHON3=/apps/intel-2020/intel-2020/intelpython3/bin/python3
 
 # NOAA RDHPCS Hera
 #HOMEhafs=/scratch1/NCEPDEV/hwrf/save/${USER}/HAFS
@@ -42,24 +43,48 @@ scrubopt="config.scrub_work=yes config.scrub_com=yes"
 
  confopts_noocean="config.EXPT=${EXPT} config.SUBEXPT=HAFS2021_ensemble \
      ../parm/hafsv2021_ensemble_AL.conf"
-
+#Jet
 #for ens in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
 #do
-
+#
 #if [ $ens -eq 00 ] ; then
 # ${PYTHON3} ./run_hafs.py -t ${dev} 2020082112 00L HISTORY \
 #     ${confopts_noocean} ${scrubopt} \
 #     config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_${ens} config.ENS=${ens} \
 #     config.do_sppt=.F. config.do_shum=.F. config.do_skeb=.F. \
-#     config.disk_project=hwrf-vd 
+#     config.disk_project=hurricane forecast.layoutx=13 forecast.layouty=12 \
+#     forecast.write_tasks_per_group=12 dir.COMgfs=/lfs4/HFIP/hwrf-data/hafs-input/GEFS
 #else
 # ${PYTHON3} ./run_hafs.py -t ${dev} 2020082112 00L HISTORY \
 #     ${confopts_noocean} ${scrubopt} \
 #     config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_${ens} config.ENS=${ens} \
-#     config.disk_project=hwrf-vd 
+#     config.disk_project=hurricane forecast.layoutx=13 forecast.layouty=12 \
+#     forecast.write_tasks_per_group=12 dir.COMgfs=/lfs4/HFIP/hwrf-data/hafs-input/GEFS
 #fi
-
+#
 #done
+
+#Orion
+#for ens in 00 01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20
+for ens in 00 01
+do
+
+if [ $ens -eq 00 ] ; then
+ ${PYTHON3} ./run_hafs.py -t ${dev} 2020082112 00L HISTORY \
+     ${confopts_noocean} ${scrubopt} \
+     config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_${ens} config.ENS=${ens} \
+     config.do_sppt=.F. config.do_shum=.F. config.do_skeb=.F. \
+     config.disk_project=hurricane forecast.layoutx=16 forecast.layouty=15 \
+     forecast.write_tasks_per_group=20 dir.COMgfs=/work/noaa/hwrf/noscrub/zzhang/GEFS
+else
+ ${PYTHON3} ./run_hafs.py -t ${dev} 2020082112 00L HISTORY \
+     ${confopts_noocean} ${scrubopt} \
+     config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_${ens} config.ENS=${ens} \
+     config.disk_project=hurricane forecast.layoutx=16 forecast.layouty=15 \
+     forecast.write_tasks_per_group=20 dir.COMgfs=/work/noaa/hwrf/noscrub/zzhang/GEFS
+fi
+
+done
 
 #===============================================================================
 
