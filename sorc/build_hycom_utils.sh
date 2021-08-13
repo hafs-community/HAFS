a349a24ba36487eab7e51b66f3e679d9a71a498c
Daniel Rosen <daniel.rosen@noaa.gov>
2021-08-13 16:06:22 +0000

    Fix typo in build_hycom_utils.sh

--- sorc/build_hycom_utils.sh
+++ sorc/build_hycom_utils.sh
@@ -23,7 +23,7 @@ if [ $target = wcoss_cray ]; then
   CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ftn}
   CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-cc}
 else
-  MAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ifort}
+  CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ifort}
   CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-icc}
 fi
 cmake .. -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
@@ -39,7 +39,7 @@ if [ $target = wcoss_cray ]; then
   CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ftn}
   CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-cc}
 else
-  MAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ifort}
+  CMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER:-ifort}
   CMAKE_C_COMPILER=${CMAKE_C_COMPILER:-icc}
 fi
 cmake .. -DCMAKE_Fortran_COMPILER=${CMAKE_Fortran_COMPILER} -DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}
