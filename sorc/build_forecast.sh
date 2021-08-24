44701f699558e8783a57796cb3016ef4b1836bd3
evankalina <evan.kalina@noaa.gov>
2021-08-24 17:51:06 +0000

    Revert changes to build_forecast.sh. We agreed to only specify -DAPP=HAFS in this file. The user can change it to -DAPP=HAFS-ALL if they want to build with CDEPS support, as described in the documentation.

--- sorc/build_forecast.sh
+++ sorc/build_forecast.sh
@@ -10,10 +10,6 @@ if [ $target = cheyenne ]; then target=cheyenne.intel ; fi
 
 cd hafs_forecast.fd/tests
 
-# For normal HAFS:
-#./compile.sh "$target" "-DAPP=HAFS -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson,FV3_HAFS_v0_hwrf -D32BIT=ON" 32bit YES NO
-
-# For normal HAFS + HAFS with data models:
-./compile.sh "$target" "-DAPP=HAFS-ALL -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson,FV3_HAFS_v0_hwrf -D32BIT=ON" 32bit YES NO
+./compile.sh "$target" "-DAPP=HAFS -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson,FV3_HAFS_v0_hwrf -D32BIT=ON" 32bit YES NO
 
 exit
