28db3fb27dab2ba7254a9b2df9b5c6342ddb402c
Bin.Liu <Bin.Liu@noaa.gov>
2022-01-13 13:00:25 -0600

    Enable the atm_init jobs running properly Currently build the forecast without moving nesting

--- sorc/build_forecast.sh
+++ sorc/build_forecast.sh
@@ -15,7 +15,7 @@ else
 fi
 cd hafs_forecast.fd/tests
 
-./compile.sh "$target" "-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson -D32BIT=ON" 32bit YES NO
-#./compile.sh "$target" "-DAPP=$app -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson -D32BIT=ON" 32bit YES NO
+#./compile.sh "$target" "-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_hwrf_thompson -D32BIT=ON" 32bit YES NO
+./compile.sh "$target" "-DAPP=HAFS -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf -D32BIT=ON" 32bit YES NO
 
 exit
