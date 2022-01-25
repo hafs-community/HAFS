394e9bf93f011b95e1bc5d79ee8650b08f7cb0ba
Bin.Liu <Bin.Liu@noaa.gov>
2022-01-25 13:02:51 -0600

    Enable running the atm_vi (vortex initialization) tasks in HAFS application workflow. Note: the VI preprocess and analysis steps now run properly, but the post-processing to interpolate the VI analysis back to restart files not working yet.

--- scripts/exhafs_atm_vi.sh
+++ scripts/exhafs_atm_vi.sh
@@ -1,19 +1,31 @@
 #!/bin/sh
 
 set -xe
+export vi_warm_start_vmax_threshold=${vi_warm_start_vmax_threshold:-20} # m/s
+export vi_bogus_vmax_threshold=${vi_bogus_vmax_threshold:-33} # m/s
+export vi_storm_env_from=${vi_storm_env_from:-init}
+export vi_storm_relocation=${vi_storm_relocation:-yes}
+export vi_storm_modification=${vi_storm_modification:-yes}
+export vi_ajust_intensity=${vi_adjust_intensity:-yes}
+export vi_ajust_size=${vi_adjust_size:-yes}
+export crfactor=${crfactor:-1.0}
 
+if [ ${vi_storm_modification} = yes ]; then
+  initopt=0
+else
+  initopt=1
+fi
+
+CDATE=${CDATE:-$YMDH}
 FGAT_MODEL=${FGAT_MODEL:-gfs}
 FGAT_HR=${FGAT_HR:-00}
 
-export MERGE_TYPE=${MERGE_TYPE:-vortexreplace}
-export RESTARTsrc=${RESTARTsrc:-"${COMhafs}/RESTART_analysis"}
-export RESTARTdst=${RESTARTdst:-"${COMhafs}/RESTART_init"}
-export RESTARTmrg=${RESTARTmrg:-"${COMhafs}/RESTART_analysis_merge"}
+MERGE_TYPE=${MERGE_TYPE:-vortexreplace}
 
-TOTAL_TASKS=${TOTAL_TASKS:-2016}
-NCTSK=${NCTSK:-12}
-NCNODE=${NCNODE:-24}
-OMP_NUM_THREADS=${OMP_NUM_THREADS:-2}
+TOTAL_TASKS=${TOTAL_TASKS:-24}
+NCTSK=${NCTSK:-24}
+NCNODE=${NCNODE:-1}
+OMP_NUM_THREADS=${OMP_NUM_THREADS:-1}
 APRUNC=${APRUNC:-"aprun -b -j1 -n${TOTAL_TASKS} -N${NCTSK} -d${OMP_NUM_THREADS} -cc depth"}
 
 # Utilities
@@ -31,51 +43,353 @@ mn=`echo $CDATE | cut -c5-6`
 dy=`echo $CDATE | cut -c7-8`
 hh=`echo $CDATE | cut -c9-10`
 
-mkdir -p ${RESTARTout}
-${NCP} -rp ${RESTARTdst}/* ${RESTARTmrg}/
+export RESTARTinp=${RESTARTinp:-${COMhafsprior}/RESTART}
+export INTCOMinit=${INTCOMinit:-${WORKhafs}/intercom/atm_init}
+export RESTARTinit=${RESTARTinit:-${WORKhafs}/intercom/RESTART_init}
+export RESTARTout=${RESTARTout:-${WORKhafs}/intercom/RESTART_vi}
+export DATA=${DATA:-${WORKhafs}/atm_vi}
 
-if [ -d ${RESTARTsrc} ] || [ -L ${RESTARTsrc} ] ; then
+cd $DATA
 
 if [ ${FGAT_HR} = 03 ]; then
-  tcvital=${WORKhafs}/tm03vit
+  cp ${WORKhafs}/tm03vit tcvitals.vi
+  gesfhr=3
 elif [ ${FGAT_HR} = 06 ]; then
-  tcvital=${WORKhafs}/tmpvit
+  cp ${WORKhafs}/tmpvit tcvitals.vi
+  gesfhr=6
 elif [ ${FGAT_HR} = 09 ]; then
-  tcvital=${WORKhafs}/tp03vit
+  cp ${WORKhafs}/tp03vit tcvitals.vi
+  gesfhr=9
 else
-  tcvital=${WORKhafs}/tmpvit
+  cp ${WORKhafs}/tmpvit tcvitals.vi
+  gesfhr=6
 fi
-if [ ${MERGE_TYPE} = vortexreplace ]; then
-  MERGE_CMD="${DATOOL} vortexreplace --tcvital=${tcvital} --vortexradius=800:900 --relaxzone=50"
-elif [ ${MERGE_TYPE} = domainmerge ]; then
-  MERGE_CMD="${DATOOL} remap"
+
+tcvital=${DATA}/tcvitals.vi
+vmax_vit=`cat ${tcvital} | cut -c68-69`
+
+#===============================================================================
+# Stage 1: Process prior cycle's vortex if exists and storm intensity is
+# stronger than vi_warm_start_vmax_threshold (e.g., 20 m/s)
+
+if [ $vmax_vit -ge $vi_warm_start_vmax_threshold ] && [ -d ${RESTARTinp} ]; then
+
+  # prep
+  work_dir=${DATA}/prep_guess
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  vortexradius=30
+  res=0.02
+  time ${DATOOL} hafsvi_preproc --in_dir=${RESTARTinp} \
+                                     --infile_date=${CDATE:0:8}.${CDATE:8:2}0000 \
+                                     --tcvital=${tcvital} \
+                                     --vortexradius=${vortexradius} --res=${res} \
+                                     --out_file=vi_inp_${vortexradius}deg${res/\./p}.bin
+  vortexradius=45
+  res=0.20
+  time ${DATOOL} hafsvi_preproc --in_dir=${RESTARTinp} \
+                                     --infile_date=${CDATE:0:8}.${CDATE:8:2}0000 \
+                                     --tcvital=${tcvital} \
+                                     --vortexradius=${vortexradius} --res=${res} \
+                                     --out_file=vi_inp_${vortexradius}deg${res/\./p}.bin
+
+  # create_trak and split
+  work_dir=${DATA}/split_guess
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  # input
+  ln -sf ${tcvital} fort.11
+  if [ -e ${COMhafsprior}/${STORM,,}${STORMID,,}.${CDATEprior}.trak.hafs.atcfunix.all ]; then
+    ln -sf ${COMhafsprior}/${STORM,,}${STORMID,,}.${CDATEprior}.trak.hafs.atcfunix.all ./trak.atcfunix.all
+    grep "^.., ${STORMID:0:2}," trak.atcfunix.all \
+      > trak.atcfunix.tmp
+    # | grep -E "^${STORMBS1^^}.,|^.${STORMBS1^^}," \
+    # | grep -E "HAFS, 00.," > trak.atcfunix.tmp
+  else
+    touch trak.atcfunix.tmp
+  fi
+  ln -sf trak.atcfunix.tmp fort.12
+  # output
+  ln -sf ./trak.fnl.all fort.30
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_create_trak_guess.x ./
+  echo ${STORMID} ${CDATE:8:2} | ./hafs_vi_create_trak_guess.x
+
+  # split
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ./trak.fnl.all fort.30
+  ln -sf ../prep_guess/vi_inp_30deg0p02.bin ./fort.26
+  ln -sf ../prep_guess/vi_inp_45deg0p20.bin ./fort.46
+  # output
+  ln -sf storm_env                     fort.56
+  ln -sf rel_inform                    fort.52
+  ln -sf vital_syn                     fort.55
+  ln -sf storm_pert                    fort.71
+  ln -sf storm_radius                  fort.85
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_split.x ./
+  gesfhr=${gesfhr:-6}
+  ibgs=0
+  iflag_cold=0
+  crfactor=${crfactor:-1.0}
+  echo ${gesfhr} $ibgs $vmax_vit $iflag_cold $crfactor | ./hafs_vi_split.x
+
+  # anl_pert
+  work_dir=${DATA}/anl_pert_guess
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ../split_guess/storm_env fort.26
+  ln -sf ../prep_guess/vi_inp_30deg0p02.bin fort.46
+  ln -sf ../split_guess/storm_pert fort.71
+  ln -sf ../split_guess/storm_radius fort.65
+  # output
+  ln -sf storm_pert_new fort.58
+  ln -sf storm_size_p fort.14
+  ln -sf storm_sym fort.23
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_anl_pert.x ./
+  basin=${pubbasin2:-AL}
+  initopt=${initopt:-0}
+  echo 6 ${basin} ${initopt} | ./hafs_vi_anl_pert.x
+
+fi
+#===============================================================================
+# Stage 2: Process current cycle's vortex from the global/parent model
+
+cd $DATA
+
+  # prep
+  work_dir=${DATA}/prep_init
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  vortexradius=30
+  res=0.02
+  time ${DATOOL} hafsvi_preproc --in_dir=${RESTARTinit} \
+                                     --infile_date=${CDATE:0:8}.${CDATE:8:2}0000 \
+                                     --tcvital=${tcvital} \
+                                     --vortexradius=${vortexradius} --res=${res} \
+                                     --out_file=vi_inp_${vortexradius}deg${res/\./p}.bin
+  vortexradius=45
+  res=0.20
+  time ${DATOOL} hafsvi_preproc --in_dir=${RESTARTinit} \
+                                     --infile_date=${CDATE:0:8}.${CDATE:8:2}0000 \
+                                     --tcvital=${tcvital} \
+                                     --vortexradius=${vortexradius} --res=${res} \
+                                     --out_file=vi_inp_${vortexradius}deg${res/\./p}.bin
+
+  # create_trak and split
+  work_dir=${DATA}/split_init
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  # input
+  ln -sf ${tcvital} fort.11
+  if [ -e ${INTCOMinit}/${STORM,,}${STORMID,,}.${CDATE}.trak.hafs.atcfunix.all ]; then
+    ln -sf ${INTCOMinit}/${STORM,,}${STORMID,,}.${CDATE}.trak.hafs.atcfunix.all ./trak.atcfunix.all
+    grep "^.., ${STORMID:0:2}," trak.atcfunix.all \
+      > trak.atcfunix.tmp
+    # | grep -E "^${STORMBS1^^}.,|^.${STORMBS1^^}," \
+    # | grep -E "HAFS, 00.," > trak.atcfunix.tmp
+  else
+    touch trak.atcfunix.tmp
+  fi
+  ln -sf trak.atcfunix.tmp fort.12
+  # output
+  ln -sf ./trak.fnl.all fort.30
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_create_trak_init.x ./
+  echo ${STORMID} ${CDATE:8:2} | ./hafs_vi_create_trak_init.x
+
+  # split
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ./trak.fnl.all fort.30
+  ln -sf ../prep_init/vi_inp_30deg0p02.bin ./fort.26
+  ln -sf ../prep_init/vi_inp_45deg0p20.bin ./fort.46
+  if [ -s ../split_guess/storm_radius ]; then
+    ln -sf ../split_guess/storm_radius ./fort.65
+  fi
+  # output
+  ln -sf storm_env                     fort.56
+  ln -sf rel_inform                    fort.52
+  ln -sf vital_syn                     fort.55
+  ln -sf storm_pert                    fort.71
+  ln -sf storm_radius                  fort.85
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_split.x ./
+  gesfhr=${gesfhr:-6}
+  # Warm start or cold start
+  if [ -s fort.65 ]; then
+    ibgs=1
+    iflag_cold=0
+  else
+    ibgs=2
+    iflag_cold=1
+  fi
+  echo ${gesfhr} $ibgs $vmax_vit $iflag_cold 1.0 | ./hafs_vi_split.x
+
+  # anl_pert
+  work_dir=${DATA}/anl_pert_init
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ../split_init/storm_env fort.26
+  ln -sf ../prep_init/vi_inp_30deg0p02.bin fort.46
+  ln -sf ../split_init/storm_pert fort.71
+  ln -sf ../split_init/storm_radius fort.65
+  # output
+  ln -sf storm_pert_new fort.58
+  ln -sf storm_size_p fort.14
+  ln -sf storm_sym fort.23
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_anl_pert.x ./
+  basin=${pubbasin2:-AL}
+  initopt=${initopt:-0}
+  echo 6 ${basin} ${initopt} | ./hafs_vi_anl_pert.x
+
+#===============================================================================
+# Stage 3:
+
+  # anl_storm
+  work_dir=${DATA}/anl_storm
+  mkdir -p ${work_dir}
+  cd ${work_dir}
+
+if [ $vmax_vit -ge $vi_bogus_vmax_threshold ] && [ ! -s ../anl_pert_guess/storm_pert_new ] ; then
+  # Bogus a storm if prior cycle does not exist and tcvital intensity >= vi_bogus_vmax_threshold (e.g., 33 m/s)
+
+  # anl_bogus
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ../split_init/storm_env fort.26
+  ln -sf ../prep_init/vi_inp_30deg0p02.bin ./fort.36
+  ln -sf ../prep_init/vi_inp_30deg0p02.bin ./fort.46 #roughness
+  ln -sf ../split_init/storm_pert fort.61
+  ln -sf ../split_init/storm_radius fort.85
+
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.71
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.72
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.73
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.74
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.75
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.76
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.77
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.78
+
+  # output
+  ln -sf storm_anl_bogus                        fort.56
+
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_anl_bogus.x ./
+  basin=${pubbasin2:-AL}
+  echo 6 ${basin} | ./hafs_vi_anl_bogus.x
+  cp -p storm_anl_bogus storm_anl
+
 else
-  echo "Error: unsupported MERGE_TYPE: ${MERGE_TYPE}"
+  # warm-start from prior cycle or cold start from global/parent model
+
+  # anl_combine
+  if [ $vmax_vit -ge $vi_warm_start_vmax_threshold ] && [ -s ../anl_pert_guess/storm_pert_new ] ; then
+    pert=guess
+  else
+    pert=init
+  fi
+  if [ $vi_storm_env_from = init ] ; then
+    senv=init
+  else
+    senv=$pert
+  fi
+  if [ $pert = init ] ; then
+    gfs_flag=0
+  else
+    gfs_flag=6
+  fi
+  if [ $ENSDA = YES ]; then
+    gfs_flag=1
+  fi
+
+  rm -f flag_file
+  # input
+  ln -sf ${tcvital} fort.11
+  ln -sf ../split_${pert}/trak.atcfunix.tmp fort.12
+  ln -sf ../split_${pert}/trak.fnl.all fort.30
+  ln -sf ../anl_pert_${pert}/storm_size_p fort.14
+  ln -sf ../anl_pert_${pert}/storm_sym fort.23
+  ln -sf ../anl_pert_${pert}/storm_pert_new fort.71
+  ln -sf ../split_${senv}/storm_env fort.26
+  ln -sf ../prep_${pert}/vi_inp_30deg0p02.bin ./fort.46 #roughness
+
+  # output
+  ln -sf storm_env_new                 fort.36
+  ln -sf storm_anl_combine             fort.56
+
+  gesfhr=${gesfhr:-6}
+  basin=${pubbasin2:-AL}
+  gfs_flag=${gfs_flag:-6}
+  initopt=${initopt:-0}
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_anl_combine.x ./
+  echo ${gesfhr} ${basin} ${gfs_flag} ${initopt} | ./hafs_vi_anl_combine.x
+  if [ -s storm_anl_combine ]; then
+    cp -p storm_anl_combine storm_anl
+  fi
+
+  # If the combined storm is weaker than the tcvital intensity, add a small
+  # fraction of the composite storm to enhance the storm intensity so that it
+  # matches the tcvital intensity
+  if [ -s flag_file ] && [ -s storm_env_new ]; then
+  # anl_enhance
+  # input
+ #ln -sf ${tcvital} fort.11
+ #ln -sf ./flag_file flag_file
+  ln -sf ../anl_pert_${pert}/storm_sym fort.23
+  ln -sf storm_env_new fort.26
+  ln -sf ../prep_${pert}/vi_inp_30deg0p02.bin ./fort.46 #roughness
+  ln -sf ../split_${pert}/storm_radius fort.85
+
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.71
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.72
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.73
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.74
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.75
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.76
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_30       fort.77
+  ln -sf ${FIXhafs}/fix_vi/hafs_storm_axisy_47 fort.78
+
+  # output
+  ln -sf storm_anl_enhance                     fort.56
+
+  basin=${pubbasin2:-AL}
+  iflag_cold=${iflag_cold:-0}
+  ln -sf ${HOMEhafs}/sorc/hafs_tools.fd/exec/hafs_vi_anl_enhance.x ./
+  echo 6 ${basin} ${iflag_cold} | ./hafs_vi_anl_enhance.x
+  cp -p storm_anl_enhance storm_anl
+
+  fi
+
+fi
+
+if [ ! -s storm_anl ]; then
+  echo "FATAL ERROR: Stage3 did not produce storm_anl"
   exit 1
 fi
-rm -f cmdfile_datool_merge
-#for var in fv_core.res.tile1 fv_tracer.res.tile1 fv_srf_wnd.res.tile1 sfc_data phy_data; 
-for var in fv_core.res.tile1 fv_tracer.res.tile1 fv_srf_wnd.res.tile1 sfc_data; 
-do
-  cat >> cmdfile_datool_merge << EOF
-  time ${MERGE_CMD} \
-    --in_grid=${RESTARTsrc}/grid_spec.nc \
-    --out_grid=${RESTARTmrg}/grid_spec.nc \
-    --in_file=${RESTARTsrc}/${PDY}.${cyc}0000.${var}.nc \
-    --out_file=${RESTARTmrg}/${PDY}.${cyc}0000.${var}.nc \
-    > datool.${var}.log
-EOF
-done
 
-chmod +x cmdfile_datool_merge
-${APRUNC} ${MPISERIAL} -m cmdfile_datool_merge
-cat datool.*.log
+# Interpolate storm_anl back to HAFS restart files
+cd $DATA
 
-else
+# # post
+# work_dir=${DATA}/post
+# mkdir -p ${work_dir}
+# cd ${work_dir}
+# vortexradius=30
+# res=0.02
+# time ${HAFS_DATOOL} hafsvi_postproc --in_dir=${RESTARTinit} \
+#                                    --infile_date=${CDATE:0:8}.${CDATE:8:2}0000 \
+#                                    --tcvital=${tcvital} \
+#                                    --vortexradius=${vortexradius} --res=${res} \
+#                                    --out_file=vi_inp_${vortexradius}deg${res/\./p}.bin
 
-echo "RESTARTsrc: ${RESTARTsrc} does not exist"
-echo "RESTARTmrg is the same as RESTARTdst"
-
-fi
+#===============================================================================
 
 exit
