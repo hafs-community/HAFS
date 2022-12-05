4680fd562e77f79b864825f8cbaf37827944e21a
Bin Liu <Bin.Liu@noaa.gov>
2022-12-05 02:27:26 +0000

    Update submodule sorc/hafs_forecast.fd and rename HAFS_v0 CCPP physics suites into HAFS_v1.

--- sorc/build_forecast.sh
+++ sorc/build_forecast.sh
@@ -13,7 +13,7 @@ app=HAFS-ALL
 
 cd hafs_forecast.fd/tests
 
-./compile.sh "$target" "-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_HAFS_v0_thompson_noahmp_nonsst,FV3_HAFS_v0_thompson_noahmp,FV3_HAFS_v0_thompson_nonsst,FV3_HAFS_v0_thompson,FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_thompson_tedmf_gfdlsf -D32BIT=ON" 32bit YES NO
-#./compile.sh "$target" "-DAPP=$app -DCCPP_SUITES=FV3_HAFS_v0_gfdlmp_tedmf_nonsst,FV3_HAFS_v0_gfdlmp_tedmf,FV3_HAFS_v0_thompson_tedmf_gfdlsf -D32BIT=ON" 32bit YES NO
+./compile.sh "$target" "-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_HAFS_v1_thompson_noahmp_nonsst,FV3_HAFS_v1_thompson_noahmp,FV3_HAFS_v1_thompson_nonsst,FV3_HAFS_v1_thompson,FV3_HAFS_v1_gfdlmp_tedmf_nonsst,FV3_HAFS_v1_gfdlmp_tedmf,FV3_HAFS_v1_thompson_tedmf_gfdlsf -D32BIT=ON" 32bit YES NO
+#./compile.sh "$target" "-DAPP=$app -DCCPP_SUITES=FV3_HAFS_v1_gfdlmp_tedmf_nonsst,FV3_HAFS_v1_gfdlmp_tedmf,FV3_HAFS_v1_thompson_tedmf_gfdlsf -D32BIT=ON" 32bit YES NO
 
 exit
