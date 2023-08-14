f4e4fe03852452d248b0de37f0a7de676e38a217
samuel.trahan <Samuel.Trahan@noaa.gov>
2023-08-14 17:53:29 +0000

    update WaterInTheWest.conf with latest working version

--- rocoto/WaterInTheWest.sh
+++ rocoto/WaterInTheWest.sh
@@ -1,13 +1,13 @@
-#! /bin/sh
+#! /bin/bash
 set -xue
 
-HOMEhafs=/scratch1/BMC/zrtrr/Samuel.Trahan/westwater/HAFS
+HOMEhafs=$( cd $( dirname ${BASH_SOURCE[0]} ) ; cd .. ; pwd -P )
 source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
 
 cd ${HOMEhafs}/rocoto
 EXPT=$(basename ${HOMEhafs})
-try=try26
-SUBEXPT=${EXPT}_$try
+SUBEXPT=${EXPT}
+conf=../parm/WaterInTheWest.conf
 opts="-t -f"
 scrubopt="config.scrub_work=no config.scrub_com=no"
 
@@ -15,4 +15,4 @@ scrubopt="config.scrub_work=no config.scrub_com=no"
  ./run_hafs.py ${opts} 2020082506-2020082512 00L HISTORY \
      config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
      config.NHRS=12 ${scrubopt} \
-     ../parm/WaterInTheWest-$try.conf
+     $conf
