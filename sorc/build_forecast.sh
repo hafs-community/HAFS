a6ba20801ccb2c992a0002efc75757b529f76ccf
samuel.trahan <Samuel.Trahan@noaa.gov>
2023-11-22 20:11:09 +0000

    include old gwd suite too

--- sorc/build_forecast.sh
+++ sorc/build_forecast.sh
@@ -9,7 +9,7 @@ if [[ $target =~ .*c5 ]] ; then target=gaea-c5 ; fi
 cd hafs_forecast.fd/tests
 
 app=HAFSW
-opts="-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_global_nest_v1 -D32BIT=ON"
+opts="-DAPP=HAFSW -DMOVING_NEST=ON -DCCPP_SUITES=FV3_global_nest_v1,FV3_global_nest_v0 -D32BIT=ON"
 ./compile.sh "$target" "$opts" 32bit intel YES NO
 
 exit
