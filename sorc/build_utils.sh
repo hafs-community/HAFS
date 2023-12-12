7273564a11c26bd7d0c043bbc6a6b2c6e79e3c76
Samuel Trahan <Samuel.Trahan@noaa.gov>
2023-12-12 18:54:47 -0500

    apply patch to utils for c5 support

--- sorc/build_utils.sh
+++ sorc/build_utils.sh
@@ -4,6 +4,13 @@ source ./machine-setup.sh > /dev/null 2>&1
 if [ $target = wcoss2 ]; then source ../versions/build.ver; fi
 cwd=$(pwd)
 
+if [[ ! -s hafs_utils.fd/modulefiles/build.gaea-c5.intel.lua ]] ; then
+    pushd hafs_utils.fd
+    patch -p1 < ../customizations/hafs_utils.fd/gaea-c5-patch.diff
+    popd
+    cp -fp customizations/hafs_utils.fd/build.gaea-c5.intel.lua hafs_utils.fd/modulefiles/build.gaea-c5.intel.lua
+fi
+
 cd hafs_utils.fd
 
 ./build_all.sh
