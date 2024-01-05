3b4de3d6239f46d90bec44303d1dbaf03f151cfa
samuel.trahan <Samuel.Trahan@noaa.gov>
2024-01-05 22:43:10 +0000

    revert accidental changes to rocoto/WaterInTheWest.sh

--- rocoto/WaterInTheWest.sh
+++ rocoto/WaterInTheWest.sh
@@ -7,16 +7,13 @@ set -x
 
 cd ${HOMEhafs}/rocoto
 EXPT=$(basename ${HOMEhafs})
+SUBEXPT=$EXPT
+conf=../parm/WaterInTheWest.conf
+opts="-t -f"
 
-for res in 768 ; do
-    SUBEXPT=gnv1C$res
-    conf=../parm/WaterInTheWestC$res.conf
-    opts="-t -f"
-    
-    scrubopt="config.scrub_work=no config.scrub_com=no"
-    # 3DEnVar with GDAS ensembles
-    ./run_hafs.py ${opts} 2023022400 00L HISTORY \
-                  config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
-                  config.NHRS=24 ${scrubopt} \
-                  $conf config.run_pygraf=yes
-done
+scrubopt="config.scrub_work=no config.scrub_com=no"
+# 3DEnVar with GDAS ensembles
+./run_hafs.py ${opts} 2023022400 00L HISTORY \
+    config.EXPT=${EXPT} config.SUBEXPT=${SUBEXPT} \
+    config.NHRS=24 ${scrubopt} \
+    $conf config.run_pygraf=yes
