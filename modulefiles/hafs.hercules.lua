040c6bd003128b701913e7d4eded9cbb5370fa7e
Biju Thomas <biju.thomas@noaa.gov>
2024-02-04 15:28:44 -0600

    Updating to spack-stack-1.6.0 on Hercules

--- modulefiles/hafs.hercules.lua
+++ modulefiles/hafs.hercules.lua
@@ -5,7 +5,7 @@ loads HAFS application level modulefile on Hercules
 load("contrib")
 load("noaatools")
 
-prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.5.0/envs/unified-env/install/modulefiles/Core")
+prepend_path("MODULEPATH", "/work/noaa/epic/role-epic/spack-stack/hercules/spack-stack-1.6.0/envs/unified-env/install/modulefiles/Core")
 
 stack_intel_ver=os.getenv("stack_intel_ver") or "2021.9.0"
 load(pathJoin("stack-intel", stack_intel_ver))
@@ -13,7 +13,7 @@ load(pathJoin("stack-intel", stack_intel_ver))
 stack_impi_ver=os.getenv("stack_impi_ver") or "2021.9.0"
 load(pathJoin("stack-intel-oneapi-mpi", stack_impi_ver))
 
-python_ver=os.getenv("python_ver") or "3.10.8"
+python_ver=os.getenv("python_ver") or "3.10.13"
 load(pathJoin("stack-python", python_ver))
 
 cmake_ver=os.getenv("cmake_ver") or "3.23.1"
@@ -34,16 +34,16 @@ load(pathJoin("hdf5", hdf5_ver))
 netcdf_c_ver=os.getenv("netcdf_c_ver") or "4.9.2"
 load(pathJoin("netcdf-c", netcdf_c_ver)) 
 
-netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.0"
+netcdf_fortran_ver=os.getenv("netcdf_fortran_ver") or "4.6.1"
 load(pathJoin("netcdf-fortran", netcdf_fortran_ver)) 
 
 parallelio_ver=os.getenv("parallelio_ver") or "2.5.10"
 load(pathJoin("parallelio", parallelio_ver))
 
-esmf_ver=os.getenv("esmf_ver") or "8.4.2"
+esmf_ver=os.getenv("esmf_ver") or "8.5.0"
 load(pathJoin("esmf", esmf_ver))
 
-fms_ver=os.getenv("fms_ver") or "2023.01"
+fms_ver=os.getenv("fms_ver") or "2023.04"
 load(pathJoin("fms",fms_ver))
 
 bacio_ver=os.getenv("bacio_ver") or "2.4.1"
@@ -61,22 +61,22 @@ load(pathJoin("g2tmpl", g2tmpl_ver))
 ip_ver=os.getenv("ip_ver") or "4.3.0"
 load(pathJoin("ip", ip_ver))
 
-sp_ver=os.getenv("sp_ver") or "2.3.3"
+sp_ver=os.getenv("sp_ver") or "2.5.0"
 load(pathJoin("sp", sp_ver))
 
 w3emc_ver=os.getenv("w3emc_ver") or "2.10.0"
 load(pathJoin("w3emc", w3emc_ver))
 
-gftl_shared_ver=os.getenv("gftl_shared_ver") or "1.5.0"
+gftl_shared_ver=os.getenv("gftl_shared_ver") or "1.6.1"
 load(pathJoin("gftl-shared", gftl_shared_ver))
 
 yafyaml_ver=os.getenv("yafyaml_ver") or "0.2.5"
 load(pathJoin("libyaml", yafyaml_ver))
 
-mapl_ver=os.getenv("mapl_ver") or "2.35.2-esmf-8.4.2"
+mapl_ver=os.getenv("mapl_ver") or "2.40.3-esmf-8.5.0"
 load(pathJoin("mapl", mapl_ver))
 
-bufr_ver=os.getenv("bufr_ver") or "12.0.0"
+bufr_ver=os.getenv("bufr_ver") or "12.0.1"
 load(pathJoin("bufr", bufr_ver))
 
 sfcio_ver=os.getenv("sfcio_ver") or "1.4.1"
@@ -91,8 +91,8 @@ load(pathJoin("libszip", szip_ver))
 wrf_io_ver=os.getenv("wrf_io_ver") or "1.2.0"
 load(pathJoin("wrf-io", wrf_io_ver))
 
-prod_util_ver=os.getenv("prod_util_ver") or "1.2.2"
-load(pathJoin("prod-util", prod_util_ver))
+prod_util_ver=os.getenv("prod_util_ver") or "2.1.1"
+load(pathJoin("prod_util", prod_util_ver))
 
 grib_util_ver=os.getenv("grib_util_ver") or "1.3.0"
 load(pathJoin("grib-util", grib_util_ver))
@@ -103,17 +103,17 @@ load(pathJoin("wgrib2", wgrib2_ver))
 nco_ver=os.getenv("nco_ver") or "5.0.6"
 load(pathJoin("nco", nco_ver))
 
-cdo_ver=os.getenv("cdo_ver") or "2.0.5"
+cdo_ver=os.getenv("cdo_ver") or "2.2.0"
 load(pathJoin("cdo", cdo_ver))
 
-mkl_ver=os.getenv("mkl_ver") or "2022.2.1"
+mkl_ver=os.getenv("mkl_ver") or "2023.1.0"
 load(pathJoin("intel-oneapi-mkl", mkl_ver))
 
 tar_ver=os.getenv("tar_ver") or "1.34"
 load(pathJoin("tar", tar_ver)) 
 
-rocoto_ver=os.getenv("rocoto_ver") or "1.3.5"
-load(pathJoin("rocoto", rocoto_ver))
+rocoto_ver=os.getenv("rocoto_ver") or "1.3.6"
+load(pathJoin("rocoto"))
 
 prepend_path("MODULEPATH", "/work/noaa/hwrf/noscrub/local/modulefiles")
 load(pathJoin("python", "wcoss2_env"))
