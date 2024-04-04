b8662712537d518489cc94d4fad1dc5dcd1393e4
Bin Liu <Bin.Liu@noaa.gov>
2024-04-04 17:56:46 +0000

    Some clean-ups for ush/hafs_mom6_gfs_forcings.py.

--- ush/hafs_mom6_gfs_forcings.py
+++ ush/hafs_mom6_gfs_forcings.py
@@ -68,7 +68,6 @@ def get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t):
     return y, m, d, h, fcst_hr
 
 if __name__ == "__main__":
- 
     # get command line args
     parser = argparse.ArgumentParser(
         description="Download the atmospheric surface fields needed to force the ocean model forecast. Files are converted from grib2 to netcdf.")
@@ -82,20 +81,12 @@ if __name__ == "__main__":
     print(args)
     date_s = args.date
     length_hours = args.length_hours
-
-    print("Preparing forcing file to cover forcast period of")
-    print(" Start: ", date_s)
-    print(" Length: ", length_hours)
-    print(' ')
-
-    date_ini = date_s
-    date_end = date_ini + timedelta(hours = length_hours)
-
     print("Preparing forcing file to cover forcast period of")
     print(" Start: ", date_s)
     print(" Length: ", length_hours)
     date_ini = date_s
     date_end = date_ini + timedelta(hours = length_hours)
+
     for type,vars in enumerate(gfs_vars):
         print(type)
         print(vars)
@@ -120,52 +111,42 @@ if __name__ == "__main__":
                     print('fdate=',fdate)
                     print('delta_t=',delta_t)
                     print('shifted_time=',shifted_time)
-
                     y, m, d, h, fcst_hr = get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t)
-                    
                     if n < np.max(np.arange(nfcst_hr)):
-
                         if (int(fcst_hr)+int(h))%2 == 0:
                             y1, m1, d1, h1, fcst_hr1 = get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t)
                             print(y1,m1,d1,h1,fcst_hr1)
                             y2, m2, d2, h2, fcst_hr2 = get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t+3)
                             print(y2,m2,d2,h2,fcst_hr2)
-
                             file_gfs1 = 'gfs_global_' +y1+m1+d1+h1+ '_f' + fcst_hr1 + '.nc'
                             file_gfs2 = 'gfs_global_' +y2+m2+d2+h2+ '_f' + fcst_hr2 + '.nc'
                             tmp_gfs_nc1 = 'gfs_global_' +y1+m1+d1+h1+ '_f' + fcst_hr1 + '_' + hafs_fcst_hr + '_' + file_var_name + '.nc'
                             tmp_gfs_nc2 = 'gfs_global_' +y2+m2+d2+h2+ '_f' + fcst_hr2 + '_' + hafs_fcst_hr + '_' + file_var_name + '.nc'
-
                             # Extracting gfs_var_name from file_gfs
                             cmd = 'ncks -v ' + gfs_var_name + ' ' + file_gfs1 + ' ' + tmp_gfs_nc1
                             print(cmd)
                             os.system(cmd)
-
                             cmd = 'ncks -v ' + gfs_var_name + ' ' + file_gfs2 + ' ' + tmp_gfs_nc2
                             print(cmd)
                             os.system(cmd)
-
                             # Open ncfiles
                             ncfile1 = nc.Dataset(tmp_gfs_nc1,'a')
                             ncfile2 = nc.Dataset(tmp_gfs_nc2,'a')
                             flux1 = ncfile1[gfs_var_name][:]
                             flux2 = ncfile2[gfs_var_name][:]
                             # Weighted average
-                            flux = 1/3 * flux1 + 2/3 * flux2 
+                            flux = 1/3 * flux1 + 2/3 * flux2
                             # write corrected flux value
                             ncfile1[gfs_var_name][:] = flux
-    
                         else:
                             y1, m1, d1, h1, fcst_hr1 = get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t+3)
                             print(y1,m1,d1,h1,fcst_hr1)
                             file_gfs1 = 'gfs_global_' +y1+m1+d1+h1+ '_f' + fcst_hr1 + '.nc'
                             tmp_gfs_nc1 = 'gfs_global_' +y1+m1+d1+h1+ '_f' + fcst_hr1 + '_' + hafs_fcst_hr + '_' + file_var_name + '.nc'
-
                             # Extracting gfs_var_name from file_gfs
                             cmd = 'ncks -v ' + gfs_var_name + ' ' + file_gfs1 + ' ' + tmp_gfs_nc1
                             print(cmd)
                             os.system(cmd)
-
                             # Open ncfile
                             ncfile1 = nc.Dataset(tmp_gfs_nc1,'a')
 
@@ -177,7 +158,6 @@ if __name__ == "__main__":
                         ncfile1.variables['time'].reference_time_description = " "
                         ncfile1.variables['time'].units = "seconds since 1970-01-01 00:00:00"
                         ncfile1.close()
-
                         tmp_gfs_nc_intp = 'gfs_global_f' + hafs_fcst_hr + '_' + file_var_name + '.nc'
                         cmd = 'cp ' + tmp_gfs_nc1 + ' ' + tmp_gfs_nc_intp
                         os.system(cmd)
@@ -202,7 +182,7 @@ if __name__ == "__main__":
                 print(cmd)
                 os.system(cmd)
                 os.system('rm ' + 'gfs_global_*f*' + file_var_name + '.nc')
-  
+
             if type == 1:
                 print('Instantaneous fields')
                 gfs_var_name = var
@@ -225,7 +205,7 @@ if __name__ == "__main__":
                     print(n)
                     print('fdate=',fdate)
                     print('delta_t=',delta_t)
-                    print('shifted_time=',shifted_time)                    
+                    print('shifted_time=',shifted_time)
                     y, m, d, h, fcst_hr = get_ymdh_fcst_hr_to_read_gfs_file(date_ini,date_iforc,fdate,delta_t)
                     #y = str(date_ini.year)
                     #m = [str(date_ini.month) if len(str(date_ini.month))>1 else '0'+str(date_ini.month)][0]
