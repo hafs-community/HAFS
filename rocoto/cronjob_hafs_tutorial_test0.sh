c0fb98d29b998cd2e3c810f04c17fbcb18455afb
BinLiu-NOAA <Bin.Liu@noaa.gov>
2024-05-23 13:04:27 -0500

    Update cronjob drivers using /work2 (Hercules) as the default/main hafs colloquium tutorial practical session platfrom.

--- rocoto/cronjob_hafs_tutorial_test0.sh
+++ rocoto/cronjob_hafs_tutorial_test0.sh
@@ -2,7 +2,7 @@
 set -x
 date
 
-HOMEhafs=${HOMEhafs:-/work/noaa/hwrf/tutorial/save/${USER}/hafs_202405}
+HOMEhafs=${HOMEhafs:-/work2/noaa/hwrf/tutorial/save/${USER}/hafs_202405}
 source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
 
 cd ${HOMEhafs}/rocoto
