a478f0396df422df689a3e9746a5452f29b1c648
Bin.Liu <Bin.Liu@noaa.gov>
2024-05-26 23:14:00 -0500

    Update HAFS tutorial test4 cronjob driver: cronjob_hafs_tutorial_test4.sh.

--- rocoto/cronjob_hafs_tutorial_test4.sh
+++ rocoto/cronjob_hafs_tutorial_test4.sh
@@ -15,17 +15,15 @@ scrubopt="config.scrub_work=no config.scrub_com=no config.archive=none"
  # and without wave coupling for 72-h forecast
  # Hurricane Ida 09L2021
  confopts="config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_tutorial_test4 ../parm/hfsa_dev.conf"
- ./run_hafs.py ${opts} 2021082712 09L HISTORY ${confopts} \
-     config.NHRS=72 ${scrubopt} config.run_emcgraphics=yes
+ ./run_hafs.py ${opts} 2021082712 09L HISTORY ${confopts} ${scrubopt} config.NHRS=72 config.run_emcgraphics=yes
 
 ## HAFS tutorial test 4a: HAFSv2A configuration with dev computation resources
-## and without wave coupling for 126-h forecast
+## and with one-way wave coupling
 ## Hurricane Ida 09L2021
-#confopts="config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_tutorial_test4a ../parm/hfsa_dev.conf"
+#confopts="config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_tutorial_test4a ../parm/hfsa_dev_ww3.conf"
 #./run_hafs.py ${opts} 2021082712-2021082718 09L HISTORY ${confopts} config.run_emcgraphics=yes
 
 ## HAFS tutorial test 4b: HAFSv2B configuration with dev computation resources
-## for 126-h forecast
 ## Hurricane Ida 09L2021
 #confopts="config.EXPT=${EXPT} config.SUBEXPT=${EXPT}_tutorial_test4b ../parm/hfsb_dev.conf"
 #./run_hafs.py ${opts} 2021082712-2021082718 09L HISTORY ${confopts} config.run_emcgraphics=yes
