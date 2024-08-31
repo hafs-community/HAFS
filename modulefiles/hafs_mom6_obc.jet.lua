7fdbf133e83ebaf964af6cb8b36e4b7115d0b914
Biju <biju.thomas@noaa.gov>
2024-08-31 10:46:16 +0000

    Updating modulefiles for OCN_OBC while using miniconda package

--- modulefiles/hafs_mom6_obc.jet.lua
+++ modulefiles/hafs_mom6_obc.jet.lua
@@ -4,6 +4,8 @@ loads HAFS/MOM6 OBC modulefile on Jet
 unload("esmf")
 unload("py-numpy")
 unload("py-xarray")
+unload("py-scipy")
+unload("py-netcdf4")
 prepend_path("PATH", "/lfs5/HFIP/hwrfv3/Maria.Aristizabal/miniconda3/envs/OBC_env/bin")
 prepend_path("PYTHONPATH", "/lfs5/HFIP/hwrfv3/Maria.Aristizabal/miniconda3/envs/OBC_env")
 setenv("ESMFMKFILE", "/lfs5/HFIP/hwrfv3/Maria.Aristizabal/miniconda3/envs/OBC_env/lib/esmf.mk")
