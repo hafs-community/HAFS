336f358486b83fb5c96c94abc536df5976ca86a9
Biju Thomas <biju.thomas@noaa.gov>
2024-10-07 10:05:37 -0500

    Sync with lf4 to lfs5 migration change on Jet

--- modulefiles/hafs_mom6_obc.hercules.lua
+++ modulefiles/hafs_mom6_obc.hercules.lua
@@ -2,8 +2,12 @@ help([[
 loads HAFS/MOM6 OBC modulefile on Hercules
 ]])
 unload("esmf")
-prepend_path("MODULEPATH", "/work/noaa/hwrf/save/maristiz/modulefiles")
-load("modulefile.OBC.run.orion")
-setenv("ESMFMKFILE", "/work/noaa/hwrf/save/maristiz/miniconda3/envs/OBC_env/lib/esmf.mk")
+unload("py-numpy")
+unload("py-xarray")
+unload("py-scipy")
+unload("py-netcdf4")
+prepend_path("PATH", "/work2/noaa/hwrf/maristiz/miniconda3/envs/OBC_env/bin")
+prepend_path("PYTHONPATH", "/work2/noaa/hwrf/maristiz/miniconda3/envs/OBC_env")
+setenv("ESMFMKFILE", "/work2/noaa/hwrf/maristiz/miniconda3/envs/OBC_env/lib/esmf.mk")
 
 whatis("Description: HAFS/MOM6 OBC  environment")
