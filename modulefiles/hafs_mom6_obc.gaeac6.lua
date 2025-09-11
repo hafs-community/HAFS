c0865f4e85d4407d7fc1a5e5347205216caa6a70
XuLu-NOAA <Xu.Lu@noaa.gov>
2025-09-11 12:09:21 -0500

    Merging HAFS-HDASApp with feature/hafsv2p2_baseline

--- modulefiles/hafs_mom6_obc.gaeac6.lua
+++ modulefiles/hafs_mom6_obc.gaeac6.lua
@@ -2,7 +2,11 @@ help([[
 loads HAFS/MOM6 OBC modulefile on Orion
 ]])
 unload("esmf")
-unload("py-numpy") 
+unload("py-numpy")
+unload("py-pandas")
+unload("py-scipy")
+unload("py-xarray")
+unload("py-netcdf4")
 prepend_path("PATH", "/ncrc/proj/hurr1/hafs/shared/miniconda3/envs/OBCmini_env/bin")
 prepend_path("PYTHONPATH", "/ncrc/proj/hurr1/hafs/shared/miniconda3/envs/OBCmini_env")
 setenv("ESMFMKFILE", "/ncrc/proj/hurr1/hafs/shared/miniconda3/envs/OBCmini_env/lib/esmf.mk")
