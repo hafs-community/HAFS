a55334e53ea8d97d5b825cb7dd35528899a19eb2
Bin Liu <Bin.Liu@noaa.gov>
2025-11-24 18:41:28 +0000

    Update submodule sorc/hafs_utils.fd. Update scripts/exhafs_atm_prep.sh to use higher resolution substrate temperature data.

--- scripts/exhafs_atm_prep.sh
+++ scripts/exhafs_atm_prep.sh
@@ -583,7 +583,7 @@ cat>./fort.41<<EOF
 &config
 !input_leaf_area_index_file="${input_sfc_climo_dir}/leaf_area_index.0.05.nc"
 input_facsf_file="${input_sfc_climo_dir}/facsf.1.0.nc"
-input_substrate_temperature_file="${input_sfc_climo_dir}/substrate_temperature.2.6x1.5.nc"
+input_substrate_temperature_file="${input_sfc_climo_dir}/substrate_temperature.gfs.0.5.nc"
 input_maximum_snow_albedo_file="${input_sfc_climo_dir}/maximum_snow_albedo.0.05.nc"
 input_snowfree_albedo_file="${input_sfc_climo_dir}/snowfree_albedo.4comp.0.05.nc"
 input_slope_type_file="${input_sfc_climo_dir}/slope_type.1.0.nc"
@@ -594,11 +594,10 @@ mosaic_file_mdl="${mosaic_file}"
 orog_dir_mdl="${out_dir}"
 orog_files_mdl=${the_orog_files}
 halo=${HALO}
-maximum_snow_albedo_method="nearest_stod"
-snowfree_albedo_method="nearest_stod"
-vegetation_greenness_method="nearest_stod"
-substrate_temperature_method="nearest_stod"
-!leaf_area_index_method="nearest_stod"
+maximum_snow_albedo_method="bilinear"
+snowfree_albedo_method="bilinear"
+vegetation_greenness_method="bilinear"
+!leaf_area_index_method="bilinear"
 /
 EOF
 
@@ -662,7 +661,7 @@ cat>./fort.41<<EOF
 &config
 !input_leaf_area_index_file="${input_sfc_climo_dir}/leaf_area_index.0.05.nc"
 input_facsf_file="${input_sfc_climo_dir}/facsf.1.0.nc"
-input_substrate_temperature_file="${input_sfc_climo_dir}/substrate_temperature.1.0.nc"
+input_substrate_temperature_file="${input_sfc_climo_dir}/substrate_temperature.gfs.0.5.nc"
 input_maximum_snow_albedo_file="${input_sfc_climo_dir}/maximum_snow_albedo.0.05.nc"
 input_snowfree_albedo_file="${input_sfc_climo_dir}/snowfree_albedo.4comp.0.05.nc"
 input_slope_type_file="${input_sfc_climo_dir}/slope_type.1.0.nc"
@@ -673,11 +672,10 @@ mosaic_file_mdl="${mosaic_file}"
 orog_dir_mdl="${out_dir}"
 orog_files_mdl=${the_orog_files}
 halo=${HALO}
-maximum_snow_albedo_method="nearest_stod"
-snowfree_albedo_method="nearest_stod"
-vegetation_greenness_method="nearest_stod"
-substrate_temperature_method="nearest_stod"
-!leaf_area_index_method="nearest_stod"
+maximum_snow_albedo_method="bilinear"
+snowfree_albedo_method="bilinear"
+vegetation_greenness_method="bilinear"
+!leaf_area_index_method="bilinear"
 /
 EOF
 
