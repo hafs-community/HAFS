07358e1583d6c91797040985fad0f11c53913880
Bin Liu <Bin.Liu@noaa.gov>
2025-12-20 04:35:48 +0000

    Update scripts/exhafs_obs_prep.sh to detect/use the obsprocv1_hdob location for combined regular and uas hdob bufr_d files.

--- scripts/exhafs_obs_prep.sh
+++ scripts/exhafs_obs_prep.sh
@@ -345,7 +345,7 @@ if [ -s ${intercom}/${NFTLDPLR} ] && [[ ${tdr_superob:-.false.} = .true. ]]; the
 fi
 
 # If exist and non-empty, use all HDOB data including regular and S2 UAS
-HDOBnew=${COMINobs}/../obsprocv1_uas/gfs.$PDY/$cyc/${atmos}/gfs.t${cyc}z.hdob.tm00.bufr_d
+HDOBnew=${COMINobs}/../obsprocv1_hdob/gfs.$PDY/$cyc/${atmos}/gfs.t${cyc}z.hdob.tm00.bufr_d
 if [ -s ${HDOBnew} ]; then
   # Deliver to intercom
   ${NCP} -L ${HDOBnew} ${intercom}/${NFHDOB}
