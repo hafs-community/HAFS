4c2489fc4e0865cc59dd0f71b15793253aa7999b
XuLu-NOAA <Xu.Lu@noaa.gov>
2025-12-24 17:48:01 +0000

    Move IAU configuration to parm files

--- scripts/exhafs_forecast.sh
+++ scripts/exhafs_forecast.sh
@@ -199,9 +199,9 @@ fi
 
 # Set default options for IAU
 iau_inc_files=","
-iau_filter_increments=.false.
-iau_delthrs=2
-iaufhrs=0
+iau_filter_increments=${iau_filter_increments:-.true.}
+iau_delthrs=${iau_delthrs:-2}
+iaufhrs=${iaufhrs:-0}
 
 # Sepcial settings if this is an atm_init forecast run
 if [ ${RUN_INIT:-NO} = YES ]; then
@@ -1361,7 +1361,6 @@ for n in $(seq 2 ${nest_grids}); do
   blocksize=$(( ${npy_nml}/${layouty_nml} ))
   if [ ${RUN_GSI:-NO} = "YES" ] && [ ${GSI_D02:-NO} = "YES" ] && \
      [ ${RUN_INIT:-NO} = "NO" ] && [ ${iau_regional:-.false.} = ".true." ]; then
-    iau_filter_increments=.true.
     iau_inc_files="analysis_inc_nest0${inest}.nc"
     # Linking increment file
     ${NLN} ${RESTARTinp}/analysis_inc_nest0${inest}.nc INPUT/
