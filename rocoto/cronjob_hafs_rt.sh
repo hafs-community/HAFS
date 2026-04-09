ca42f66f135277e27a19324306e4be428fbb5f90
Bin Liu <Bin.Liu@noaa.gov>
2026-04-09 19:42:32 +0000

    Remove HAFS application level support for RDHPCS Jet.

--- rocoto/cronjob_hafs_rt.sh
+++ rocoto/cronjob_hafs_rt.sh
@@ -2,7 +2,7 @@
 set -x
 date
 
-HOMEhafs=${HOMEhafs:-/lfs/h2/emc/hur/noscrub/${USER}/save/HAFS}
+HOMEhafs=${HOMEhafs:-/lfs/h2/emc/hur/noscrub/${USER}/save/hafsv2p2_20260409}
 source ${HOMEhafs}/ush/hafs_pre_job.sh.inc
 
 cd ${HOMEhafs}/rocoto
--- sorc/link_fix.sh
+++ sorc/link_fix.sh
@@ -19,8 +19,6 @@ elif [ ${target} == "hera" ]; then
   FIXROOT=/scratch3/HFIP/hwrfv3/noscrub/hafs-fix-files/hafs-${FIXversion}-fix/fix
 elif [ ${target} == "orion" ] || [ ${target} == "hercules" ]; then
   FIXROOT=/work/noaa/hwrf/noscrub/hafs-fix-files/hafs-${FIXversion}-fix/fix
-elif [ ${target} == "jet" ]; then
-  FIXROOT=/lfs5/HFIP/hwrf-data/hafs-fix-files/hafs-${FIXversion}-fix/fix 
 elif [ ${target} == "gaeac6" ]; then
   FIXROOT=/gpfs/f6/drsa-hurr1/world-shared/noscrub/hafs-fix-files/hafs-${FIXversion}-fix/fix
 elif [ ${target} == "ursa" ]; then
