76eed96f7aeb9f094fcc86fc9ef482fa2c81c59e
Bin Liu <Bin.Liu@noaa.gov>
2026-09-18 17:43:16 +0000

    Sync NCO/SPA's latest HAFSv2.2 updates.  - Set warmstart as default for coupled atm-ocn forecast jobs in ecf scripts.  - Fix typos in scripts/exhafs_atm_ic.sh and scripts/exhafs_forecast.sh.

--- scripts/exhafs_forecast.sh
+++ scripts/exhafs_forecast.sh
@@ -453,7 +453,7 @@ elif [ $gtype = nest ]; then
     ATM_tasks=$(($ATM_tasks+$write_groups*$write_tasks_per_group))
   fi
 else
-  echo "FATAL ERROR: Unsupported gtype of ${gtype}. Currently onnly support gtype of nest or regional."
+  echo "FATAL ERROR: Unsupported gtype of ${gtype}. Currently only support gtype of nest or regional."
   exit 9
 fi
 
@@ -1477,7 +1477,7 @@ if [ $gtype = regional ]; then
 elif [ $gtype = nest ]; then
   ngrids=$(( ${nest_grids} + 1 ))
 else
-  echo "FATAL ERROR: Unsupported gtype of ${gtype}. Currently onnly support gtype of nest or regional."
+  echo "FATAL ERROR: Unsupported gtype of ${gtype}. Currently only support gtype of nest or regional."
   exit 9
 fi
 
